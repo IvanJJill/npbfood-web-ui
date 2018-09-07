d12179604681780afa2d35f9019cd98e00e1fa8c
Ivan <ivanjjill@gmail.com>
2018-09-08 01:07:59 +0200

    working on look and feel

--- src/components/MealDishes.js
+++ src/components/MealDishes.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Label } from 'semantic-ui-react';
+import { Card, Image, Label, List } from 'semantic-ui-react';
 
 export default ({ data }) => {
   return data.dishes.map((dish, idx) => (
-    <Label size="big" as="a" color="blue" image key={idx + dish.name}>
-      <img src="https://react.semantic-ui.com/images/avatar/small/veronika.jpg" />
-      {dish.name}
-      <Label.Detail>{dish.amount}</Label.Detail>
-    </Label>
+    <Card key={idx + dish.name}>
+      <Card.Content>
+        <Image
+          floated="left"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+        />
+        <Card.Header>{dish.name}</Card.Header>
+        <Card.Meta>{dish.amount + 'g'}</Card.Meta>
+        <Card.Description>Oat Flakes, Milk, Honey</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Label as="a" color="teal" circular>
+          Info
+        </Label>
+        <List horizontal relaxed>
+          <List.Item />
+          <List.Item content={dish.amount + 'g'} />
+          <List.Item content={dish.portions + 'pts'} />
+
+          {dish.ingredients ? (
+            <List.Item color="grey">
+              {dish.ingredients.length + ' ' + 'ing'}
+            </List.Item>
+          ) : null}
+        </List>
+      </Card.Content>
+    </Card>
   ));
 };
--- src/components/MealRow.js
+++ src/components/MealRow.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { GridColumn, GridRow, Divider, Icon } from 'semantic-ui-react';
+import { GridColumn, GridRow, Card, Icon } from 'semantic-ui-react';
 import MealDishes from './MealDishes';
 
 export default ({ mealName, mealData }) => {
   return (
-    <GridRow>
+    <GridRow columns="3">
       <GridColumn width={4} textAlign="right" verticalAlign="middle" size="23">
         <h3>{mealName}</h3>
       </GridColumn>
-      <Divider vertical />
       <GridColumn width={1} textAlign="center" verticalAlign="middle">
         <Icon name="plus circle" size="large" color="green" />
       </GridColumn>
       <GridColumn width={11}>
-        <MealDishes data={mealData} />
+        <Card.Group itemsPerRow={4}>
+          <MealDishes data={mealData} />
+        </Card.Group>
       </GridColumn>
     </GridRow>
   );
